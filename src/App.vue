543731fde74d19ae472f20cae77699324e8b2275
Jolman Gonzalez <jolmangonzalez5@gmail.com>
2024-09-18 17:38:24 -0500

    app

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,47 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <header>
+      <img src="./assets/pokemon.png" />
+    </header>
+    <div class="pokebola">
+      <img src="./assets/pokebola.png" />
+    </div>
+
+    <main>
+      <div class="pokemon" v-for="pokemon in pokemones" :key="pokemon.id">
+        <img :src="pokemon.imagen" alt="" />
+        <h2>{{ pokemon.nombre }}</h2>
+        <div class="power">
+          <p>{{ pokemon.type_name }}</p>
+        </div>
+      </div>
+    </main>
+  </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+import axios from "axios";
 export default {
-  name: "App",
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      pokemones: [],
+    };
+  },
+  create() {
+    this.getPokemon();
+  },
+  methods: {
+    async getPokemon() {
+      try {
+        const response = await axios.get(
+          "https://cobuses.com.co/APIV2/model/pokemon.php?dato=getallpokemon"
+        );
+        this.pokemones = response.data;
+        console.log(response.data);
+      } catch (error) {
+        console.error("El error es: " + error);
+      }
+    },
   },
 };
 </script>
@@ -21,6 +53,33 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+header {
+  width: 100%;
+  height: 100px;
+  background: #000;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+header img {
+  height: 90%;
+}
+
+.pokebola {
+  width: 100%;
+  height: 60px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.pokebola img {
+  height: 90%;
 }
 </style>
